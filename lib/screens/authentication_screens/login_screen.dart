dfe0c000a14dae488c65d3d5a6f174452ecf8419
Masume Çetin <masume.cetin@veripark.com>
2025-03-04 00:56:23 +0300

    add : assets - structure
    add : assets - structure

---
+++ lib/screens/authentication_screens/login_screen.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class MyStatefulWidget extends StatefulWidget {
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stateful Widget Example'),
+      ),
+      body: Center(
+        child: Text('Empty Stateful Widget'),
+      ),
+    );
+  }
+}
